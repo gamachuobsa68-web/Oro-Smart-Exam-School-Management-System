674aff754f37e33c7309800089d1f3fff0449ab1
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 16:10:44 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from django.urls import path, include
+from django.urls import (
+    path,
+    include
+)
 
 
 from rest_framework_simplejwt.views import (
@@ -12,45 +15,80 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
 
+
+    # Django Admin Panel
     path(
         "admin/",
         admin.site.urls
     ),
 
 
+
+    # JWT Login
     path(
         "api/token/",
-        TokenObtainPairView.as_view()
+        TokenObtainPairView.as_view(),
+        name="token"
     ),
 
 
+
+    # JWT Refresh
     path(
         "api/token/refresh/",
-        TokenRefreshView.as_view()
+        TokenRefreshView.as_view(),
+        name="token-refresh"
     ),
 
 
+
+    # User Account System
     path(
         "api/accounts/",
-        include("accounts.urls")
+        include(
+            "accounts.urls"
+        )
     ),
 
 
+
+    # Exam System
     path(
         "api/exams/",
-        include("exams.urls")
+        include(
+            "exams.urls"
+        )
     ),
 
 
+
+    # Report Card System
     path(
         "api/reports/",
-        include("reports.urls")
+        include(
+            "reports.urls"
+        )
     ),
 
 
+
+    # Student Registration + Roster
     path(
         "api/students/",
-        include("students.urls")
+        include(
+            "students.urls"
+        )
     ),
+
+
+
+    # Teacher Assistant + Lesson Plan + CPD
+    path(
+        "api/professional/",
+        include(
+            "professional.urls"
+        )
+    ),
+
 
 ]
