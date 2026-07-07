14746884a6a34cae7e407123614d60f95fa0ab94
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 13:41:32 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
+
 from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
 
 
 urlpatterns = [
 
-    path("admin/", admin.site.urls),
+    path(
+        "admin/",
+        admin.site.urls
+    ),
 
-    path("api/accounts/", include("accounts.urls")),
+
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view()
+    ),
+
+
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view()
+    ),
+
+
+    path(
+        "api/accounts/",
+        include("accounts.urls")
+    ),
 
 ]
