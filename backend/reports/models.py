0cdcde7b9b29f0d3e6f10ad35a8af75eb0796314
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 14:52:15 +0300

    Create models.py

---
+++ backend/reports/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+
+from accounts.models import User
+
+from school.models import AcademicYear
+
+
+
+class ReportCard(models.Model):
+
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+    academic_year = models.ForeignKey(
+        AcademicYear,
+        on_delete=models.CASCADE
+    )
+
+
+    total_mark = models.FloatField(
+        default=0
+    )
+
+
+    average = models.FloatField(
+        default=0
+    )
+
+
+    rank = models.IntegerField(
+        default=0
+    )
+
+
+    grade = models.CharField(
+        max_length=5,
+        blank=True
+    )
+
+
+    status = models.CharField(
+        max_length=20,
+        default="FAIL"
+    )
+
+
+    teacher_comment = models.TextField(
+        blank=True
+    )
+
+
+    principal_comment = models.TextField(
+        blank=True
+    )
+
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def save(self, *args, **kwargs):
+
+        if self.average >= 80:
+            self.grade = "A"
+
+        elif self.average >= 70:
+            self.grade = "B"
+
+        elif self.average >= 60:
+            self.grade = "C"
+
+        elif self.average >= 50:
+            self.grade = "D"
+
+        else:
+            self.grade = "F"
+
+
+        if self.average >= 50:
+            self.status = "PASS"
+
+        else:
+            self.status = "FAIL"
+
+
+        super().save(*args, **kwargs)
+
+
+
+    def __str__(self):
+
+        return self.student.username
