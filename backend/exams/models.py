98a901179e23621f8810a791385c632392508c6c
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 14:19:50 +0300

    Update models.py

--- backend/exams/models.py
+++ backend/exams/models.py
@@ -1,22 +1,19 @@
 from django.db import models
 
+from accounts.models import User
+
 from school.models import (
     Subject,
     ClassRoom
 )
-
-from accounts.models import User
 
 
 
 class Exam(models.Model):
 
     EXAM_TYPES = (
-
         ("ONLINE", "Online"),
-
         ("PAPER", "Paper"),
-
     )
 
 
@@ -39,7 +36,8 @@ class Exam(models.Model):
 
     teacher = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="created_exams"
     )
 
 
@@ -71,7 +69,6 @@ class Exam(models.Model):
 
 
     def __str__(self):
-
         return self.title
 
 
@@ -80,13 +77,9 @@ class Exam(models.Model):
 
 class Question(models.Model):
 
-
     QUESTION_TYPES = (
-
         ("MCQ", "Multiple Choice"),
-
         ("TEXT", "Written Answer"),
-
     )
 
 
@@ -136,11 +129,127 @@ class Question(models.Model):
 
 
     correct_answer = models.CharField(
-        max_length=255,
-        blank=True
+        max_length=255
     )
 
 
     def __str__(self):
-
         return self.question_text
+
+
+
+
+
+class StudentAnswer(models.Model):
+
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
+
+    answer = models.CharField(
+        max_length=255
+    )
+
+
+    is_correct = models.BooleanField(
+        default=False
+    )
+
+
+    mark_obtained = models.IntegerField(
+        default=0
+    )
+
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def __str__(self):
+        return self.student.username
+
+
+
+
+
+class ExamResult(models.Model):
+
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+    exam = models.ForeignKey(
+        Exam,
+        on_delete=models.CASCADE
+    )
+
+
+    total_mark = models.IntegerField(
+        default=0
+    )
+
+
+    percentage = models.FloatField(
+        default=0
+    )
+
+
+    grade = models.CharField(
+        max_length=5,
+        blank=True
+    )
+
+
+    status = models.CharField(
+        max_length=20,
+        default="FAIL"
+    )
+
+
+    submitted_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def save(self, *args, **kwargs):
+
+        if self.percentage >= 80:
+            self.grade = "A"
+
+        elif self.percentage >= 70:
+            self.grade = "B"
+
+        elif self.percentage >= 60:
+            self.grade = "C"
+
+        elif self.percentage >= 50:
+            self.grade = "D"
+
+        else:
+            self.grade = "F"
+
+
+        if self.percentage >= 50:
+            self.status = "PASS"
+
+        else:
+            self.status = "FAIL"
+
+
+        super().save(*args, **kwargs)
+
+
+
+    def __str__(self):
+        return self.student.username
