714b2e63e7ab4e09a95b98f5e38e8fc91d026506
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 13:58:54 +0300

    Create serializers.py

---
+++ backend/exams/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from .models import (
+    Exam,
+    Question
+)
+
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Question
+
+        fields = "__all__"
+
+
+
+
+class ExamSerializer(serializers.ModelSerializer):
+
+    questions = QuestionSerializer(
+        many=True,
+        read_only=True
+    )
+
+
+    class Meta:
+
+        model = Exam
+
+        fields = "__all__"
