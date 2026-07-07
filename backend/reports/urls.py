07a8d61a352b9c6289f0655f5855833d18e79f5c
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 15:34:02 +0300

    Update urls.py

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -1,23 +1,44 @@
 from django.urls import path
 
+
 from .views import (
+
+    AdminReportView,
+
+    TeacherReportView,
+
+    StudentReportView,
+
     DownloadReportCardPDF
+
 )
 
 
 
-urlpatterns=[
+urlpatterns = [
 
 
     path(
-
-        "download/<int:report_id>/",
-
-        DownloadReportCardPDF.as_view(),
-
-        name="download-report"
-
+        "admin/",
+        AdminReportView.as_view()
     ),
 
+
+    path(
+        "teacher/",
+        TeacherReportView.as_view()
+    ),
+
+
+    path(
+        "student/",
+        StudentReportView.as_view()
+    ),
+
+
+    path(
+        "download/<int:report_id>/",
+        DownloadReportCardPDF.as_view()
+    ),
 
 ]
