af3f970f6ae440ea1b97212f47f9028a0c5dc535
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 15:45:22 +0300

    Create serializers.py

---
+++ backend/students/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import StudentProfile
+
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = StudentProfile
+
+        fields = "__all__"
