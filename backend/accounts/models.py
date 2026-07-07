9f64c1d145c77fdec81b50aedaa8b65938f76ad8
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 16:19:56 +0300

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,38 +1,34 @@
 from django.contrib.auth.models import AbstractUser
+
 from django.db import models
+
 
 
 class User(AbstractUser):
 
+
     ROLE_CHOICES = (
 
-        ("ADMIN", "Admin"),
+        ("ADMIN","Admin"),
 
-        ("TEACHER", "Teacher"),
+        ("TEACHER","Teacher"),
 
-        ("STUDENT", "Student"),
+        ("STUDENT","Student"),
 
     )
+
 
 
     role = models.CharField(
+
         max_length=20,
+
         choices=ROLE_CHOICES,
+
         default="STUDENT"
+
     )
 
-
-    phone = models.CharField(
-        max_length=20,
-        blank=True
-    )
-
-
-    profile_image = models.ImageField(
-        upload_to="profiles/",
-        blank=True,
-        null=True
-    )
 
 
     def __str__(self):
