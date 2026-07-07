bf7b8bb7b287bfba7743c76133a20663925df03c
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 13:45:33 +0300

    Create permissions.py

---
+++ backend/accounts/permissions.py
@@ -0,0 +1,34 @@
+from rest_framework.permissions import BasePermission
+
+
+
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+
+        return (
+            request.user.is_authenticated
+            and request.user.role == "ADMIN"
+        )
+
+
+
+class IsTeacher(BasePermission):
+
+    def has_permission(self, request, view):
+
+        return (
+            request.user.is_authenticated
+            and request.user.role == "TEACHER"
+        )
+
+
+
+class IsStudent(BasePermission):
+
+    def has_permission(self, request, view):
+
+        return (
+            request.user.is_authenticated
+            and request.user.role == "STUDENT"
+        )
