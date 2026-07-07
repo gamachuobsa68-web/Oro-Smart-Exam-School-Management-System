068e5614d6ff1433aa0c1162a6d58c8b239661b9
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 15:05:20 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from django.urls import path, include
+from django.urls import (
+    path,
+    include
+)
+
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -8,36 +12,63 @@ from rest_framework_simplejwt.views import (
 )
 
 
+
 urlpatterns = [
 
+
+    # Django Admin
     path(
         "admin/",
         admin.site.urls
     ),
 
 
+
+    # JWT Login
     path(
         "api/token/",
-        TokenObtainPairView.as_view()
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair"
     ),
 
 
+
+    # JWT Refresh Token
     path(
         "api/token/refresh/",
-        TokenRefreshView.as_view()
+        TokenRefreshView.as_view(),
+        name="token_refresh"
     ),
 
 
+
+    # User Accounts
     path(
         "api/accounts/",
-        include("accounts.urls")
+        include(
+            "accounts.urls"
+        )
     ),
 
 
+
+    # Exam System
     path(
         "api/exams/",
-        include("exams.urls")
+        include(
+            "exams.urls"
+        )
     ),
 
-]
 
+
+    # Report Card System
+    path(
+        "api/reports/",
+        include(
+            "reports.urls"
+        )
+    ),
+
+
+]
