a2db332d414ec17bf98d608e01969c3d13f0c385
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 13:37:36 +0300

    Create views.py

---
+++ backend/accounts/views.py
@@ -0,0 +1,24 @@
+from rest_framework.views import APIView
+
+from rest_framework.response import Response
+
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import UserSerializer
+
+
+
+class ProfileView(APIView):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+
+    def get(self, request):
+
+        user = request.user
+
+        return Response(
+            UserSerializer(user).data
+        )
