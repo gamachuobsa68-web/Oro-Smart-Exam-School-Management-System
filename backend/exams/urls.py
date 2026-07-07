f05f21fa207429b84bcceea3373eea2010c9b6cf
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 14:49:32 +0300

    Update urls.py

--- backend/exams/urls.py
+++ backend/exams/urls.py
@@ -3,29 +3,52 @@ from django.urls import path
 from .views import (
     TeacherExamView,
     StudentExamView,
-    SubmitExamView
+    StartExamView,
+    SaveAnswerView,
+    SubmitExamView,
 )
 
 
 
 urlpatterns = [
 
-
+    # Teacher create and view exams
     path(
         "teacher/",
-        TeacherExamView.as_view()
+        TeacherExamView.as_view(),
+        name="teacher-exams"
     ),
 
 
+    # Student view published exams
     path(
         "student/",
-        StudentExamView.as_view()
+        StudentExamView.as_view(),
+        name="student-exams"
     ),
 
 
+    # Student start exam
+    path(
+        "start/",
+        StartExamView.as_view(),
+        name="start-exam"
+    ),
+
+
+    # Save answer button
+    path(
+        "save-answer/",
+        SaveAnswerView.as_view(),
+        name="save-answer"
+    ),
+
+
+    # Submit exam
     path(
         "submit/",
-        SubmitExamView.as_view()
+        SubmitExamView.as_view(),
+        name="submit-exam"
     ),
 
 ]
