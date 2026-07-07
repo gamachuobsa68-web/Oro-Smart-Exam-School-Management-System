7b5e17e8c127f7554e4cb9d36378a2af450c3446
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-07 13:54:21 +0300

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,14 +1,38 @@
 from django.urls import path
 
-from .views import ProfileView
+from .views import (
+    ProfileView,
+    AdminDashboardView,
+    TeacherDashboardView,
+    StudentDashboardView
+)
 
 
 
 urlpatterns = [
 
+
     path(
         "profile/",
         ProfileView.as_view()
+    ),
+
+
+    path(
+        "admin-dashboard/",
+        AdminDashboardView.as_view()
+    ),
+
+
+    path(
+        "teacher-dashboard/",
+        TeacherDashboardView.as_view()
+    ),
+
+
+    path(
+        "student-dashboard/",
+        StudentDashboardView.as_view()
     ),
 
 ]
