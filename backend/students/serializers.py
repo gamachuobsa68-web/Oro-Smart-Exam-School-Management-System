5023cc8ba8042aecb051a6041fdab1cb087270c2
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 12:46:34 +0300

    Update serializers.py

--- backend/students/serializers.py
+++ backend/students/serializers.py
@@ -6,8 +6,84 @@ from .models import StudentProfile
 
 class StudentSerializer(serializers.ModelSerializer):
 
+
+    username = serializers.CharField(
+
+        source="user.username",
+
+        read_only=True
+
+    )
+
+
+    first_name = serializers.CharField(
+
+        source="user.first_name",
+
+        read_only=True
+
+    )
+
+
+    last_name = serializers.CharField(
+
+        source="user.last_name",
+
+        read_only=True
+
+    )
+
+
+
     class Meta:
+
 
         model = StudentProfile
 
-        fields = "__all__"
+
+        fields = [
+
+            "id",
+
+            "user",
+
+            "username",
+
+            "first_name",
+
+            "last_name",
+
+            "student_id",
+
+            "classroom",
+
+            "photo",
+
+            "parent_name",
+
+            "parent_phone",
+
+            "address",
+
+            "date_of_birth",
+
+            "gender",
+
+            "is_active",
+
+            "created_at",
+
+            "updated_at",
+
+        ]
+
+
+        read_only_fields = [
+
+            "id",
+
+            "created_at",
+
+            "updated_at",
+
+        ]
