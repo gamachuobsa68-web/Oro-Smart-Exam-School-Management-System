cff8b1d4cc04de3f81e413c97c8fb776996e61d5
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 13:10:37 +0300

    Update serializers.py

--- backend/reports/serializers.py
+++ backend/reports/serializers.py
@@ -6,20 +6,63 @@ from .models import ReportCard
 
 class ReportCardSerializer(serializers.ModelSerializer):
 
+
+    student_username = serializers.CharField(
+
+        source="student.username",
+
+        read_only=True
+
+    )
+
+
     class Meta:
+
 
         model = ReportCard
 
+
         fields = [
+
             "id",
+
             "student",
+
+            "student_username",
+
             "academic_year",
+
             "total_mark",
+
             "average",
+
             "rank",
+
             "grade",
+
             "status",
+
             "teacher_comment",
+
             "principal_comment",
+
             "created_at",
+
+            "updated_at",
+
+        ]
+
+
+        read_only_fields = [
+
+            "id",
+
+            "grade",
+
+            "status",
+
+            "created_at",
+
+            "updated_at",
+
         ]
