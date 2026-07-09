2c8a5d603ee461d1b0d0aa56b20c7bf291120981
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 13:56:36 +0300

    Create urls.py

---
+++ backend/cpd/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .views import (
+    CPDListView,
+    CPDActivityView
+)
+
+urlpatterns = [
+
+    path(
+        "",
+        CPDListView.as_view(),
+        name="cpd-plans"
+    ),
+
+    path(
+        "activities/",
+        CPDActivityView.as_view(),
+        name="cpd-activities"
+    ),
+
+]
