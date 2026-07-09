75b2dcfbd4fcdc24cfcf4389e54b5fe00fc1a606
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 12:38:38 +0300

    Update admin.py

--- backend/teachers/admin.py
+++ backend/teachers/admin.py
@@ -3,4 +3,48 @@ from django.contrib import admin
 from .models import Teacher
 
 
-admin.site.register(Teacher)
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+
+
+    list_display = (
+
+        "user",
+
+        "employee_id",
+
+        "is_active",
+
+        "created_at"
+
+    )
+
+
+    list_filter = (
+
+        "is_active",
+
+    )
+
+
+    search_fields = (
+
+        "user__username",
+
+        "employee_id",
+
+        "user__first_name",
+
+        "user__last_name"
+
+    )
+
+
+    filter_horizontal = (
+
+        "subjects",
+
+        "classrooms",
+
+    )
