752afcca78ce97f46ec1925631a58dcf51c71704
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 09:09:00 +0300

    Update screens.py

--- mobile/screens.py
+++ mobile/screens.py
@@ -1,1 +1,15 @@
+from kivy.uix.screenmanager import Screen
 
+
+class LoginScreen(Screen):
+    def login(self):
+        username = self.ids.username.text
+        password = self.ids.password.text
+
+        # yeroo jalqabaa test qofa
+        if username and password:
+            self.manager.current = "home"
+
+
+class HomeScreen(Screen):
+    pass
