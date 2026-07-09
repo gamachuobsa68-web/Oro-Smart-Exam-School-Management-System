71207a0ffc0470a200e749dd505e93d3076a5c54
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 14:33:41 +0300

    Create urls.py

---
+++ backend/assistant/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+
+from .views import AssistantView
+
+
+
+urlpatterns = [
+
+
+    path(
+
+        "",
+
+        AssistantView.as_view(),
+
+        name="assistant"
+
+    ),
+
+]
