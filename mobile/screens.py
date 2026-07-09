9684b3baccdbcd2a2f8efd9a77776feefd2e3698
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 11:12:27 +0300

    Update screens.py

--- mobile/screens.py
+++ mobile/screens.py
@@ -10,6 +10,10 @@ from api import (
 
 
 
+# =========================
+# LOGIN SCREEN
+# =========================
+
 class LoginScreen(Screen):
 
     def login_user(self):
@@ -18,7 +22,15 @@ class LoginScreen(Screen):
         password = self.ids.password.text
 
 
-        if login(username, password):
+        success = login(
+            username,
+            password
+        )
+
+
+        if success:
+
+            self.ids.message.text = ""
 
             self.manager.current = "home"
 
@@ -30,26 +42,38 @@ class LoginScreen(Screen):
 
 
 
+# =========================
+# HOME / STUDENT DASHBOARD
+# =========================
+
 class HomeScreen(Screen):
+
 
     def on_pre_enter(self):
 
         exams = get_student_exams()
 
-        text = ""
+
+        self.ids.exam_list.text = ""
 
 
-        for exam in exams:
+        if exams:
 
-            text += (
-                str(exam["id"])
-                + " - "
-                + exam["title"]
-                + "\n"
+            for exam in exams:
+
+                self.ids.exam_list.text += (
+                    str(exam["id"])
+                    + " - "
+                    + exam["title"]
+                    + "\n"
+                )
+
+        else:
+
+            self.ids.exam_list.text = (
+                "No published exams"
             )
 
-
-        self.ids.exam_list.text = text
 
 
 
@@ -57,50 +81,155 @@ class HomeScreen(Screen):
 
         exam_id = self.ids.exam_id.text
 
-        self.manager.exam_id = exam_id
 
-        self.manager.current = "exam"
+        if exam_id:
+
+            self.manager.exam_id = int(
+                exam_id
+            )
+
+            self.manager.current = "exam"
 
 
 
 
+
+# =========================
+# EXAM SCREEN
+# =========================
 
 class ExamScreen(Screen):
+
 
     def on_pre_enter(self):
 
         exam_id = self.manager.exam_id
 
-        data = start_exam(exam_id)
 
-        self.manager.exam_data = data
-
-
-        question = data["questions"][0]
-
-
-        self.ids.question.text = question["question_text"]
-
-        self.ids.a.text = question["option_a"]
-        self.ids.b.text = question["option_b"]
-        self.ids.c.text = question["option_c"]
-        self.ids.d.text = question["option_d"]
-
-
-
-    def send_answer(self, answer):
-
-        save_answer(
-            self.manager.exam_id,
-            self.manager.exam_data["questions"][0]["id"],
-            answer
+        data = start_exam(
+            exam_id
         )
 
 
-    def finish(self):
+        if data:
+
+            self.manager.exam_data = data
+
+
+            questions = data.get(
+                "questions",
+                []
+            )
+
+
+            if questions:
+
+                self.manager.questions = questions
+
+                self.manager.question_index = 0
+
+
+                self.show_question()
+
+
+
+
+    def show_question(self):
+
+        index = self.manager.question_index
+
+
+        question = (
+            self.manager.questions[index]
+        )
+
+
+        self.ids.question.text = (
+            question["question_text"]
+        )
+
+
+        self.ids.a.text = (
+            question.get(
+                "option_a",
+                ""
+            )
+        )
+
+
+        self.ids.b.text = (
+            question.get(
+                "option_b",
+                ""
+            )
+        )
+
+
+        self.ids.c.text = (
+            question.get(
+                "option_c",
+                ""
+            )
+        )
+
+
+        self.ids.d.text = (
+            question.get(
+                "option_d",
+                ""
+            )
+        )
+
+
+
+
+    def answer(self, value):
+
+        index = self.manager.question_index
+
+
+        question = (
+            self.manager.questions[index]
+        )
+
+
+        save_answer(
+
+            self.manager.exam_id,
+
+            question["id"],
+
+            value
+
+        )
+
+
+
+        if index + 1 < len(
+            self.manager.questions
+        ):
+
+            self.manager.question_index += 1
+
+            self.show_question()
+
+
+        else:
+
+            self.ids.question.text = (
+                "Exam finished. Submit."
+            )
+
+
+
+
+    def submit(self):
 
         result = submit_exam(
             self.manager.exam_id
         )
 
-        self.ids.result.text = str(result)
+
+        self.ids.result.text = str(
+            result
+                )
