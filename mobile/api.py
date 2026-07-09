fd04dd266078dd0106908f445a982705bcc1135e
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 11:04:07 +0300

    Update api.py

--- mobile/api.py
+++ mobile/api.py
@@ -1,39 +1,70 @@
 import requests
 
 
-BASE_URL = "http://YOUR_SERVER_IP:8000"
+# Yeroo production ta'u server kee itti jijjiirta
+BASE_URL = "http://127.0.0.1:8000"
 
 
-def login_user(username, password):
-    url = BASE_URL + "/api/login/"
+token = None
+
+
+
+def login(username, password):
+
+    global token
+
+    url = BASE_URL + "/api/token/"
 
     data = {
         "username": username,
         "password": password
     }
 
-    try:
-        response = requests.post(url, json=data)
 
-        if response.status_code == 200:
-            return response.json()
-
-        return None
-
-    except Exception:
-        return None
+    response = requests.post(
+        url,
+        json=data
+    )
 
 
-def get_exams():
-    url = BASE_URL + "/api/exams/"
+    if response.status_code == 200:
 
-    try:
-        response = requests.get(url)
+        token = response.json()["access"]
 
-        if response.status_code == 200:
-            return response.json()
+        return True
 
-        return []
 
-    except Exception:
-        return []
+    return False
+
+
+
+
+
+def get_headers():
+
+    return {
+        "Authorization":
+        f"Bearer {token}"
+    }
+
+
+
+
+
+def get_student_exams():
+
+    url = BASE_URL + "/api/exams/student/exams/"
+
+
+    response = requests.get(
+        url,
+        headers=get_headers()
+    )
+
+
+    if response.status_code == 200:
+
+        return response.json()
+
+
+    return []
