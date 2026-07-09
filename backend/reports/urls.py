5f96bf060c396873190727e45dc1887b09db8b83
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 13:15:10 +0300

    Update urls.py

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -1,11 +1,28 @@
 from django.urls import path
-from .views import DownloadReportCardPDF
+
+
+from .views import (
+
+    DownloadReportCardPDF,
+
+)
+
 
 
 urlpatterns = [
+
+
+    # Download student report card PDF
+
     path(
+
         "report-card/<int:student_id>/",
+
         DownloadReportCardPDF,
-        name="download_report_card"
+
+        name="download-report-card"
+
     ),
+
+
 ]
