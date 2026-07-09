38cc5c4767cef7d5dc4b822ee44db81dda9dd823
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 11:05:43 +0300

    Update screens.py

--- mobile/screens.py
+++ mobile/screens.py
@@ -1,15 +1,58 @@
 from kivy.uix.screenmanager import Screen
 
+from api import login, get_student_exams
+
+
 
 class LoginScreen(Screen):
-    def login(self):
+
+    def login_user(self):
+
         username = self.ids.username.text
         password = self.ids.password.text
 
-        # yeroo jalqabaa test qofa
-        if username and password:
+
+        success = login(
+            username,
+            password
+        )
+
+
+        if success:
+
             self.manager.current = "home"
+
+        else:
+
+            self.ids.message.text = "Login failed"
+
+
+
 
 
 class HomeScreen(Screen):
-    pass
+
+
+    def on_pre_enter(self):
+
+        exams = get_student_exams()
+
+
+        text = "Available Exams\n\n"
+
+
+        if exams:
+
+            for exam in exams:
+
+                text += (
+                    exam["title"]
+                    + "\n"
+                )
+
+        else:
+
+            text += "No exams found"
+
+
+        self.ids.exam_list.text = text
