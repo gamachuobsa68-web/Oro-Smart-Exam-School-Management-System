d8a757213fdb692df7add87c8bade477e802d222
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 11:16:40 +0300

    Update main.py

--- mobile/main.py
+++ mobile/main.py
@@ -1,28 +1,67 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
-from screens import LoginScreen, HomeScreen
+from screens import (
+    LoginScreen,
+    HomeScreen,
+    ExamScreen
+)
+
 
 
 class OroExamManager(ScreenManager):
-    pass
+
+    def __init__(self, **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.exam_id = None
+
+        self.exam_data = None
+
+        self.questions = []
+
+        self.question_index = 0
+
+
+
 
 
 class OroSmartExam(App):
 
+
     def build(self):
+
         sm = OroExamManager()
 
-        sm.add_widget(
-            LoginScreen(name="login")
-        )
 
         sm.add_widget(
-            HomeScreen(name="home")
+            LoginScreen(
+                name="login"
+            )
         )
+
+
+        sm.add_widget(
+            HomeScreen(
+                name="home"
+            )
+        )
+
+
+        sm.add_widget(
+            ExamScreen(
+                name="exam"
+            )
+        )
+
 
         return sm
 
 
+
+
+
 if __name__ == "__main__":
+
     OroSmartExam().run()
