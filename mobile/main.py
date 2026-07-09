42242765b1df57807a24828247eab0157a2fc6ba
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 09:04:59 +0300

    Update main.py

--- mobile/main.py
+++ mobile/main.py
@@ -1,1 +1,26 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
+
+class LoginScreen(Screen):
+    def login(self):
+        self.manager.current = "home"
+
+
+class HomeScreen(Screen):
+    pass
+
+
+class ExamApp(App):
+    def build(self):
+        Builder.load_file("ui.kv")
+
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen(name="login"))
+        sm.add_widget(HomeScreen(name="home"))
+        return sm
+
+
+if __name__ == "__main__":
+    ExamApp().run()
