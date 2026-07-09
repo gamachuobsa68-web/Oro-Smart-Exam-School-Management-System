f2468ccc6f22785fcb5e9d77232a41a69e673fdd
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 15:34:57 +0300

    Update Views.py

--- backend/cpd/Views.py
+++ backend/cpd/Views.py
@@ -1,115 +1,66 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from accounts.permissions import (
-    IsAdmin,
-    IsTeacher
-)
-
 from .models import (
     CPDPlan,
     CPDActivity
 )
 
-from .serializers import (
-    CPDPlanSerializer,
-    CPDActivitySerializer
-)
 
-
-
-# =========================
-# CPD PLAN
-# =========================
 
 class CPDListView(APIView):
-
-    permission_classes = [
-        IsAdmin
-    ]
-
 
     def get(self, request):
 
         plans = CPDPlan.objects.all()
 
-        serializer = CPDPlanSerializer(
-            plans,
-            many=True
-        )
+        data = []
 
-        return Response(
-            serializer.data
-        )
+        for plan in plans:
+
+            data.append({
+
+                "id": plan.id,
+
+                "title": plan.title,
+
+                "description": plan.description,
+
+                "month": plan.month
+
+            })
 
 
-    def post(self, request):
-
-        serializer = CPDPlanSerializer(
-            data=request.data
-        )
-
-        if serializer.is_valid():
-
-            serializer.save()
-
-            return Response(
-                serializer.data,
-                status=201
-            )
-
-        return Response(
-            serializer.errors,
-            status=400
-        )
+        return Response(data)
 
 
 
-# =========================
-# CPD ACTIVITY
-# =========================
+
 
 class CPDActivityView(APIView):
 
-    permission_classes = [
-        IsTeacher
-    ]
-
-
     def get(self, request):
 
-        activities = CPDActivity.objects.filter(
-            teacher=request.user
-        )
+        activities = CPDActivity.objects.all()
 
-        serializer = CPDActivitySerializer(
-            activities,
-            many=True
-        )
-
-        return Response(
-            serializer.data
-        )
+        data = []
 
 
-    def post(self, request):
+        for activity in activities:
 
-        serializer = CPDActivitySerializer(
-            data=request.data
-        )
+            data.append({
 
-        if serializer.is_valid():
+                "plan": activity.plan.title,
 
-            serializer.save(
-                teacher=request.user
-            )
+                "teacher": activity.teacher,
 
-            return Response(
-                serializer.data,
-                status=201
-            )
+                "activity": activity.activity,
 
-        return Response(
-            serializer.errors,
-            status=400
-        )
+                "date": activity.date,
+
+                "status": activity.status
+
+            })
+
+
+        return Response(data)
