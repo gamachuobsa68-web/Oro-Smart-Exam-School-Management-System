e9173e6a83255bc76f9e701df57eafe680cd7719
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 10:28:53 +0300

    Update api.py

--- mobile/api.py
+++ mobile/api.py
@@ -1,1 +1,39 @@
+import requests
 
+
+BASE_URL = "http://YOUR_SERVER_IP:8000"
+
+
+def login_user(username, password):
+    url = BASE_URL + "/api/login/"
+
+    data = {
+        "username": username,
+        "password": password
+    }
+
+    try:
+        response = requests.post(url, json=data)
+
+        if response.status_code == 200:
+            return response.json()
+
+        return None
+
+    except Exception:
+        return None
+
+
+def get_exams():
+    url = BASE_URL + "/api/exams/"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.json()
+
+        return []
+
+    except Exception:
+        return []
