3e000f149c0fbbe80d41fd227d6adfc9196062fa
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 13:33:01 +0300

    Update urls.py

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -13,26 +13,47 @@ from .views import (
 
 
 
-urlpatterns=[
+urlpatterns = [
 
+
+    # Admin Dashboard
 
     path(
+
         "admin/",
-        AdminDashboardView.as_view()
+
+        AdminDashboardView.as_view(),
+
+        name="admin-dashboard"
+
     ),
 
 
 
+    # Teacher Dashboard
+
     path(
+
         "teacher/",
-        TeacherDashboardView.as_view()
+
+        TeacherDashboardView.as_view(),
+
+        name="teacher-dashboard"
+
     ),
 
 
 
+    # Student Dashboard
+
     path(
+
         "student/",
-        StudentDashboardView.as_view()
+
+        StudentDashboardView.as_view(),
+
+        name="student-dashboard"
+
     ),
 
 ]
