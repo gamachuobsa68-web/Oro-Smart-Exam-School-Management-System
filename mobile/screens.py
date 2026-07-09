0e5d939bc613b2190dc838500bc5444057c12227
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-09 11:10:37 +0300

    Update screens.py

--- mobile/screens.py
+++ mobile/screens.py
@@ -1,6 +1,12 @@
 from kivy.uix.screenmanager import Screen
 
-from api import login, get_student_exams
+from api import (
+    login,
+    get_student_exams,
+    start_exam,
+    save_answer,
+    submit_exam
+)
 
 
 
@@ -12,13 +18,7 @@ class LoginScreen(Screen):
         password = self.ids.password.text
 
 
-        success = login(
-            username,
-            password
-        )
-
-
-        if success:
+        if login(username, password):
 
             self.manager.current = "home"
 
@@ -32,27 +32,75 @@ class LoginScreen(Screen):
 
 class HomeScreen(Screen):
 
-
     def on_pre_enter(self):
 
         exams = get_student_exams()
 
+        text = ""
 
-        text = "Available Exams\n\n"
 
+        for exam in exams:
 
-        if exams:
-
-            for exam in exams:
-
-                text += (
-                    exam["title"]
-                    + "\n"
-                )
-
-        else:
-
-            text += "No exams found"
+            text += (
+                str(exam["id"])
+                + " - "
+                + exam["title"]
+                + "\n"
+            )
 
 
         self.ids.exam_list.text = text
+
+
+
+    def open_exam(self):
+
+        exam_id = self.ids.exam_id.text
+
+        self.manager.exam_id = exam_id
+
+        self.manager.current = "exam"
+
+
+
+
+
+class ExamScreen(Screen):
+
+    def on_pre_enter(self):
+
+        exam_id = self.manager.exam_id
+
+        data = start_exam(exam_id)
+
+        self.manager.exam_data = data
+
+
+        question = data["questions"][0]
+
+
+        self.ids.question.text = question["question_text"]
+
+        self.ids.a.text = question["option_a"]
+        self.ids.b.text = question["option_b"]
+        self.ids.c.text = question["option_c"]
+        self.ids.d.text = question["option_d"]
+
+
+
+    def send_answer(self, answer):
+
+        save_answer(
+            self.manager.exam_id,
+            self.manager.exam_data["questions"][0]["id"],
+            answer
+        )
+
+
+    def finish(self):
+
+        result = submit_exam(
+            self.manager.exam_id
+        )
+
+        self.ids.result.text = str(result)
